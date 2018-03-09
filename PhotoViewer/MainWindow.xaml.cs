4a056a09b359438a493b7e343393972898d217e8
Хамелеон <maxali946@gmail.com>
2018-03-09 14:07:04 +0400

    95%

--- PhotoViewer/MainWindow.xaml.cs
+++ PhotoViewer/MainWindow.xaml.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Forms;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using WinForms = System.Windows.Forms;
-
+//---------------------------------------------------------------------------------------
 namespace PhotoViewer
 {
     /// <summary>
@@ -19,16 +20,17 @@ namespace PhotoViewer
 
         System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
 
+        //---------------------------------------------------------------------------------------
         public MainWindow()
         {
             InitializeComponent();
 
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
 
             InfoListBox.Visibility = Visibility.Hidden;
         }
-
+        //---------------------------------------------------------------------------------------
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
             InfoListBox.Items.Clear();
@@ -48,16 +50,29 @@ namespace PhotoViewer
 
             InfoListBox.Items.Add(box);
         }
-
+        //---------------------------------------------------------------------------------------
         private void FolderOpen_Click(object sender, RoutedEventArgs e)
         {
-            WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog();
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
             folderDialog.ShowNewFolderButton = false;
-            //            folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
-            WinForms.DialogResult result = folderDialog.ShowDialog();
+            //folderDialog.SelectedPath = System.AppDomain.CurrentDomain.BaseDirectory;
+            DialogResult result = folderDialog.ShowDialog();
 
             if (result == WinForms.DialogResult.OK)
             {
+                if (list.Count != 0)
+                {
+                    InfoListBox.Items.Clear(); 
+
+                    PhotoButtonPanel.Children.RemoveAt(list.Count - 1);
+
+                    list.Clear();
+
+                    ImageFrame.Source = null; 
+
+                    current = 0;
+                }
+
                 String spath = folderDialog.SelectedPath;
                 FolderPath.Text = spath;
 
@@ -71,7 +86,7 @@ namespace PhotoViewer
                     {
                         list.Add(item);
 
-                        Button button = new Button();
+                        System.Windows.Controls.Button button = new System.Windows.Controls.Button();
 
                         button.Margin = new Thickness(10);
 
@@ -79,7 +94,11 @@ namespace PhotoViewer
 
                         button.Background = new ImageBrush(new BitmapImage(new Uri(item.FullName)));
 
-                        button.Name = count.ToString();
+                        //string name = item.Name.Trim(new char[] {' ', '(', });
+
+                        //string name = item.Name.Replace('.', ' ').Replace('(',' ').Replace(')',' ').Replace(' ','_');
+
+                        button.Name = "Pic" + count;
 
                         PhotoButtonPanel.Children.Add(button);
 
@@ -98,38 +117,84 @@ namespace PhotoViewer
                 box.Content += "Creation time:\t" + file.CreationTime;
 
                 InfoListBox.Items.Add(box);
+
+
             }
-
         }
+        //---------------------------------------------------------------------------------------
+        private void AddNewPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
 
+                //openFileDialog.Multiselect = true;
+
+                openFileDialog.Filter = "Image files (*.jpg)|*.jpg|(*.png)|*.png";
+
+                if (openFileDialog.ShowDialog() == WinForms.DialogResult.OK)
+                {
+                    string fileName = openFileDialog.FileName;
+
+                    fileName = fileName.Substring(fileName.LastIndexOf('\\') + 1);
+
+                    string newPathFile = FolderPath.Text + '\\' + fileName;
+
+                    FileInfo file = new FileInfo(newPathFile);
+
+                    Directory.Move(openFileDialog.FileName, newPathFile);
+
+                    list.Add(file);
+
+                    System.Windows.Controls.Button button = new System.Windows.Controls.Button();
+
+                    button.Margin = new Thickness(10);
+
+                    button.Width = 60;
+
+                    button.Background = new ImageBrush(new BitmapImage(new Uri(newPathFile)));
+
+                    button.Name = "Pic" + (list.Count - 1);
+
+                    PhotoButtonPanel.Children.Add(button);
+                }
+            }
+        }
+        //---------------------------------------------------------------------------------------
         private void OlderPhoto_Click(object sender, RoutedEventArgs e)
         {
-            InfoListBox.Items.Clear();
+            if (list.Count != 0)
+            {
+                InfoListBox.Items.Clear();
 
-            if (current > 0)
-                current--;
+                if (current > 0)
+                    current--;
 
-            FileInfo file = new FileInfo(list[current].FullName);
+                FileInfo file = new FileInfo(list[current].FullName);
 
-            ImageFrame.Source = new BitmapImage(new Uri(file.ToString()));
+                ImageFrame.Source = new BitmapImage(new Uri(file.ToString()));
 
-            ListBoxItem box = new ListBoxItem();
-            box.Content += "Attributes:\t" + file.Attributes + "\n";
-            box.Content += "Size:\t\t" + file.Length + "b\n";
-            box.Content += "Name:\t\t" + file.Name + "\n";
-            box.Content += "Creation time:\t" + file.CreationTime;
+                ListBoxItem box = new ListBoxItem();
+                box.Content += "Attributes:\t" + file.Attributes + "\n";
+                box.Content += "Size:\t\t" + file.Length + "b\n";
+                box.Content += "Name:\t\t" + file.Name + "\n";
+                box.Content += "Creation time:\t" + file.CreationTime;
 
-            InfoListBox.Items.Add(box);
+                InfoListBox.Items.Add(box);
+            }
         }
-
+        //---------------------------------------------------------------------------------------
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
-            dispatcherTimer.Start();
+            if (list.Count != 0)
+            {
+                dispatcherTimer.Start();
 
-            PlayButton.Visibility = Visibility.Hidden;
-            StopButton.Visibility = Visibility.Visible;
+                PlayButton.Visibility = Visibility.Hidden;
+                StopButton.Visibility = Visibility.Visible;
+            }
         }
-
+        //---------------------------------------------------------------------------------------
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
             dispatcherTimer.Stop();
@@ -137,27 +202,46 @@ namespace PhotoViewer
             StopButton.Visibility = Visibility.Hidden;
             PlayButton.Visibility = Visibility.Visible;
         }
-
+        //---------------------------------------------------------------------------------------
         private void NextPhoto_Click(object sender, RoutedEventArgs e)
         {
-            InfoListBox.Items.Clear();
+            if (list.Count != 0)
+            {
+                InfoListBox.Items.Clear();
 
-            if (current < list.Count - 1)
-                current++;
+                if (current < list.Count - 1)
+                    current++;
 
-            FileInfo file = new FileInfo(list[current].FullName);
+                FileInfo file = new FileInfo(list[current].FullName);
 
-            ImageFrame.Source = new BitmapImage(new Uri(file.ToString()));
+                ImageFrame.Source = new BitmapImage(new Uri(file.ToString()));
 
-            ListBoxItem box = new ListBoxItem();
-            box.Content += "Attributes:\t" + file.Attributes + "\n";
-            box.Content += "Size:\t\t" + file.Length + "b\n";
-            box.Content += "Name:\t\t" + file.Name + "\n";
-            box.Content += "Creation time:\t" + file.CreationTime;
+                ListBoxItem box = new ListBoxItem();
+                box.Content += "Attributes:\t" + file.Attributes + "\n";
+                box.Content += "Size:\t\t" + file.Length + "b\n";
+                box.Content += "Name:\t\t" + file.Name + "\n";
+                box.Content += "Creation time:\t" + file.CreationTime;
 
-            InfoListBox.Items.Add(box);
+                InfoListBox.Items.Add(box);
+            }
         }
+        //---------------------------------------------------------------------------------------
+        private void PhotoButtonPanel_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                System.Windows.Controls.Button button = e.Source as System.Windows.Controls.Button;
 
+                int num = Convert.ToInt16(button.Name.Remove(0, 3));
+
+                FileInfo file = new FileInfo(list[num].FullName);
+
+                ImageFrame.Source = new BitmapImage(new Uri(file.ToString()));
+
+                current = num;
+            }
+        }
+        //---------------------------------------------------------------------------------------
         private void InfoAboutPhoto_Click(object sender, RoutedEventArgs e)
         {
             if (InfoListBox.Visibility == Visibility.Hidden)
@@ -165,18 +249,7 @@ namespace PhotoViewer
             else
                 InfoListBox.Visibility = Visibility.Hidden;
         }
-
-        private void AddNewPhoto_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = new Button();
-
-            button.Margin = new Thickness(10);
-
-            button.Width = 60;
-
-            PhotoButtonPanel.Children.Add(button);
-        }
-
+        //---------------------------------------------------------------------------------------
         bool MouseDownClick = false;
 
         private void ImageFrame_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -194,14 +267,6 @@ namespace PhotoViewer
                 TreeView.Width = 130;
             }
         }
-
-        private void PhotoButtonPanel_Click(object sender, RoutedEventArgs e)
-        {
-            Button button = e.OriginalSource as Button;
-
-            MessageBox.Show(button.Name.ToString());
-
-            //ImageFrame.Source = new BitmapImage(new Uri(button.ToString()));
-        }
     }
 }
+//---------------------------------------------------------------------------------------
